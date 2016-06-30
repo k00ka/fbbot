30802c2de5384b09522a97f4dfca6b24a4462585
David Andrews <david@ryatta.com>
2016-06-30 16:45:28 -0400

    Hardening.

--- bot2.rb
+++ bot2.rb
@@ -31,8 +31,10 @@ end
 
 # default approach: accumulate everything, ignoring confidence
 def merge(session_id, context, entities, msg)
-  entities.each_pair { |k,v| context[k.to_s] = v.first["value"] }
-  STDERR.puts "Merged Wit entities '#{entities}' into context '#{context}'"
+  unless entities.nil?
+    entities.each_pair { |k,v| context[k.to_s] = v.first["value"] }
+    STDERR.puts "Merged Wit entities '#{entities}' into context '#{context}'"
+  end
   # This is one way to handle derived context
   case context["sentiment"]
   when 'positive'
@@ -50,7 +52,7 @@ def clear_context(session_id, context)
 end
 
 $JOKES = {
-"Logical" => [
+"logic" => [
   "There are only 10 types of people in the world: those that understand binary and those that don’t.",
   "Computers make very fast, very accurate mistakes.",
   "Be nice to the nerds, for all you know they might be the next Bill Gates!",
@@ -58,12 +60,12 @@ $JOKES = {
   "To err is human – and to blame it on a computer is even more so.",
   "CAPS LOCK – Preventing Login Since 1980." ],
 
-"Browsing" => [
+"the web" => [
   "The truth is out there. Anybody got the URL?",
   "The Internet: where men are men, women are men, and children are FBI agents.",
   "Some things Man was never meant to know. For everything else, there’s Google." ],
 
-"Operating Systems" => [
+"operating systems" => [
   "The box said ‘Requires Windows Vista or better’. So I installed LINUX.",
   "UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity.",
   "In a world without fences and walls, who needs Gates and Windows?",
@@ -80,7 +82,7 @@ $JOKES = {
   "Computers are like air conditioners: they stop working when you open Windows.",
   "Mac users swear by their Mac, PC users swear at their PC." ],
  
-"Programming" => [
+"development" => [
   "If at first you don’t succeed; call it version 1.0.",
   "My software never has bugs. It just develops random features.",
   "I would love to change the world, but they won’t give me the source code.",
@@ -99,7 +101,14 @@ $JOKES = {
   "My attitude isn’t bad. It’s in beta.",
   "Get the Beta joke on a T-Shirt from the MakeUseOf T-Shirt store." ],
 
-"General" => [
+"computations" => [
+  "There are three kinds of people: those who can count and those who can’t.",
+  "Latest survey shows that 3 out of 4 people make up 75% of the world’s population.",
+  "Hand over the calculator, friends don’t let friends derive drunk.",
+  "An infinite crowd of mathematicians enters a bar. The first one orders a pint, the second one a half pint, the third one a quarter pint… “I understand”, says the bartender – and pours two pints.",
+  "1f u c4n r34d th1s u r34lly n33d t0 g37 l41d." ],
+
+"computing" => [
   "Enter any 11-digit prime number to continue.",
   "E-mail returned to sender, insufficient voltage.",
   "All wiyht. Rho sritched mg kegtops awound?",
@@ -109,21 +118,23 @@ $JOKES = {
   "SUPERCOMPUTER: what it sounded like before you bought it.",
   "Evolution is God’s way of issuing upgrades.",
   "Linus Torvalds: “Real men don’t use backups, they post their stuff on a public ftp server and let the rest of the world make copies.”",
-  "Hacking is like sex. You get in, you get out, and hope that you didn’t leave something that can be traced back to you." ],
-
-"Calculations" => [
-  "There are three kinds of people: those who can count and those who can’t.",
-  "Latest survey shows that 3 out of 4 people make up 75% of the world’s population.",
-  "Hand over the calculator, friends don’t let friends derive drunk.",
-  "An infinite crowd of mathematicians enters a bar. The first one orders a pint, the second one a half pint, the third one a quarter pint… “I understand”, says the bartender – and pours two pints.",
-  "1f u c4n r34d th1s u r34lly n33d t0 g37 l41d." ]
+  "Hacking is like sex. You get in, you get out, and hope that you didn’t leave something that can be traced back to you." ]
 }
 
 def fetch_joke(session_id, context)
   $contexts[session_id] = context
-  category = context["category"] || "default"
+  category = (context["category"] || $JOKES.keys.last).downcase
+  return fetch_categories(session_id, context) unless $JOKES.keys.include? category
   STDERR.puts "Finding a joke with Wit context '#{context}'"
-  $contexts[session_id]["joke"] = $JOKES[category].sample
+  $contexts[session_id]["response"] = $JOKES[category].sample
+  $contexts[session_id]
+end
+
+def fetch_categories(session_id, context)
+  $contexts[session_id] = context
+  categories = $JOKES.keys.join(', ')
+  STDERR.puts "Returning the list of joke categories: #{categories}"
+  $contexts[session_id]["response"] = "I know jokes in the following categories: #{categories}"
   $contexts[session_id]
 end
 
@@ -140,7 +151,8 @@ $actions = {
   say: method(:say),
   error: method(:error),
   merge: method(:merge),
+  clear_context: method(:clear_context),
   fetch_joke: method(:fetch_joke),
-  clear_context: method(:clear_context)
+  fetch_categories: method(:fetch_categories)
 }
 $wit = Wit.new(ARGV.shift, $actions, Logger.new(STDERR))
