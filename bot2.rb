46827e3c1dd874581c34fe08c25f50d34e86a8c9
David Andrews <david@ryatta.com>
2016-06-30 16:17:23 -0400

    implement clear_context, update the jokes.

--- bot2.rb
+++ bot2.rb
@@ -13,6 +13,7 @@ $contexts = Hash.new({})
 
 Bot.on :message do |message|
   session_id = message.sender['id'] # use the FB sender ID as the Wit session
+  STDERR.puts "Got '#{message}' from '#{session_id}' on Facebook. Forwarding to Wit"
   $wit.run_actions(session_id, message.text, $contexts[session_id])
 end
 
@@ -45,20 +46,77 @@ def merge(session_id, context, entities, msg)
 end
 
 def clear_context(session_id, context)
+  $contexts[session_id] = {}
 end
 
 $JOKES = {
-  "chuck" => [
-    'Chuck Norris counted to infinity - twice.',
-    'Death once had a near-Chuck Norris experience.'
-  ],
-  "tech" => [
-    'Did you hear about the two antennas that got married? The ceremony was long and boring, but the reception was great!',
-    'Why do geeks mistake Halloween and Christmas? Because oct(31) == dec(25).'
-  ],
-  "default" => [
-    'Why was the Math book sad? Because it had so many problems.'
-  ],
+"Logical" => [
+  "There are only 10 types of people in the world: those that understand binary and those that don’t.",
+  "Computers make very fast, very accurate mistakes.",
+  "Be nice to the nerds, for all you know they might be the next Bill Gates!",
+  "Artificial intelligence usually beats real stupidity.",
+  "To err is human – and to blame it on a computer is even more so.",
+  "CAPS LOCK – Preventing Login Since 1980." ],
+
+"Browsing" => [
+  "The truth is out there. Anybody got the URL?",
+  "The Internet: where men are men, women are men, and children are FBI agents.",
+  "Some things Man was never meant to know. For everything else, there’s Google." ],
+
+"Operating Systems" => [
+  "The box said ‘Requires Windows Vista or better’. So I installed LINUX.",
+  "UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity.",
+  "In a world without fences and walls, who needs Gates and Windows?",
+  "C://dos, C://dos.run, run.dos.run",
+  "Bugs come in through open Windows.",
+  "Penguins love cold, they wont survive the sun.",
+  "Unix is user friendly. It’s just selective about who its friends are.",
+  "Failure is not an option. It comes bundled with your Microsoft product.",
+  "NT is the only OS that has caused me to beat a piece of hardware to death with my bare hands.",
+  "My daily Unix command list: unzip; strip; touch; finger; mount; fsck; more; yes; unmount; sleep.",
+  "Microsoft: “You’ve got questions. We’ve got dancing paperclips.”",
+  "Erik Naggum: “Microsoft is not the answer. Microsoft is the question. NO is the answer.”",
+  "Windows isn’t a virus, viruses do something.",
+  "Computers are like air conditioners: they stop working when you open Windows.",
+  "Mac users swear by their Mac, PC users swear at their PC." ],
+ 
+"Programming" => [
+  "If at first you don’t succeed; call it version 1.0.",
+  "My software never has bugs. It just develops random features.",
+  "I would love to change the world, but they won’t give me the source code.",
+  "The code that is the hardest to debug is the code that you know cannot possibly be wrong.",
+  "Beware of programmers that carry screwdrivers.",
+  "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.",
+  "The beginning of the programmer’s wisdom is understanding the difference between getting program to run and having a runnable program.",
+  "I’m not anti-social; I’m just not user friendly.",
+  "Hey! It compiles! Ship it!",
+  "If Ruby is not and Perl is the answer, you don’t understand the question.",
+  "The more I C, the less I see.",
+  "COBOL programmers understand why women hate periods.",
+  "Michael Sinz: “Programming is like sex, one mistake and you have to support it for the rest of your life.”",
+  "If you give someone a program, you will frustrate them for a day; if you teach them how to program, you will frustrate them for a lifetime.",
+  "Programmers are tools for converting caffeine into code.",
+  "My attitude isn’t bad. It’s in beta.",
+  "Get the Beta joke on a T-Shirt from the MakeUseOf T-Shirt store." ],
+
+"General" => [
+  "Enter any 11-digit prime number to continue.",
+  "E-mail returned to sender, insufficient voltage.",
+  "All wiyht. Rho sritched mg kegtops awound?",
+  "Black holes are where God divided by zero.",
+  "If I wanted a warm fuzzy feeling, I’d antialias my graphics!",
+  "If brute force doesn’t solve your problems, then you aren’t using enough.",
+  "SUPERCOMPUTER: what it sounded like before you bought it.",
+  "Evolution is God’s way of issuing upgrades.",
+  "Linus Torvalds: “Real men don’t use backups, they post their stuff on a public ftp server and let the rest of the world make copies.”",
+  "Hacking is like sex. You get in, you get out, and hope that you didn’t leave something that can be traced back to you." ],
+
+"Calculations" => [
+  "There are three kinds of people: those who can count and those who can’t.",
+  "Latest survey shows that 3 out of 4 people make up 75% of the world’s population.",
+  "Hand over the calculator, friends don’t let friends derive drunk.",
+  "An infinite crowd of mathematicians enters a bar. The first one orders a pint, the second one a half pint, the third one a quarter pint… “I understand”, says the bartender – and pours two pints.",
+  "1f u c4n r34d th1s u r34lly n33d t0 g37 l41d." ]
 }
 
 def fetch_joke(session_id, context)
